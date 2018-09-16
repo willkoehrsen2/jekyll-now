422ec7b00436231eafab291a24b6b25a16b839bd
Will K <wjk68@case.edu>
2018-09-16 12:19:13 -0400

    Updated medium to markdown conversion

--- medium_to_markdown.py
+++ medium_to_markdown.py
@@ -3,23 +3,32 @@ import subprocess
 
 if __name__ == "__main__":
     post_url = input('Enter post url:')
+    # Title of post
     title = '-'.join(post_url.split('/')[-1].split('-')[:-1])
     date = input('Enter date (as 2018-10-05):')
-
-    with open('mediumtomarkdown.js', 'r') as f:
+    # Read in the template
+    with open('medium-to-markdown.js', 'r') as f:
         template = f.readlines()
 
+    # Copy the template
     template_mod = copy.deepcopy(template)
 
+    # Update the js script with the url
     template_mod[2] = f'mediumToMarkdown.convertFromUrl("{post_url}")'
 
-    with open('mediumtomarkdown_mod.js', 'w') as f:
+    # Write the new file
+    with open('medium-to-markdown_mod.js', 'w') as f:
         f.writelines(template_mod)
 
+    # Directory for saving post
+    # File is automatically correctly named
     post_dir = f'_posts/{date}-{title}.md'
 
-    r = subprocess.call([f'node mediumtomarkdown_mod.js >> {post_dir}'],
-                        shell = True)
+    # Run the new script
+    r = subprocess.call([f'node medium-to-markdown_mod.js >> {post_dir}'],
+                        shell=True)
+
+    # If no errors, report save location
     if r == 0:
         print(f'Post saved as markdown to {post_dir}')
     else:
